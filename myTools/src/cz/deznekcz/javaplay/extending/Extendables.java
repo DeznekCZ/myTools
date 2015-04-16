ac05cd054d8bd6f07b38035bb175b17a6d483a44
DeznekCZ <peugeot.55@seznam.cz>
2015-04-16 23:58:40 +0200

    AnyTimeUpdate

--- myTools/src/cz/deznekcz/javaplay/extending/Extendables.java
+++ myTools/src/cz/deznekcz/javaplay/extending/Extendables.java
@@ -3,7 +3,7 @@ package cz.deznekcz.javaplay.extending;
 public class Extendables {
 
 	public static void main(String[] args) {
-		Child c = new Child();
+		Parent c = new Child();
 		
 		c.callOutIn();
 
