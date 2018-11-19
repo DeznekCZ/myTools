829ab69b8057faaebef4541447e84f7ead050f6f
Zdenek Novotny (DeznekCZ) <Z634227@PLZC00937.emea.zf-world.com>
2018-11-19 18:22:58 +0100

    Adding Where argument

--- myTools/cz.deznekcz.myTools.src/cz/deznekcz/util/xml/XMLStepper.java
+++ myTools/cz.deznekcz.myTools.src/cz/deznekcz/util/xml/XMLStepper.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
@@ -15,6 +16,7 @@ import org.w3c.dom.NodeList;
 
 import cz.deznekcz.reference.OutString;
 import cz.deznekcz.util.ForEach;
+import javafx.util.Pair;
 
 
 /**
@@ -34,16 +36,33 @@ public class XMLStepper {
 		private Step parent;
 		private List<Node> list;
 		private List<Step> stepList;
+		private String listValueName;
 
 
 		public StepList(String listValueName, NodeList childNodes, Step parent) {
 			this.list = new ArrayList<>();
 			this.stepList = new ArrayList<>();
+			this.listValueName = listValueName;
 			for (Node node : ForEach.DOMNodeIterable(childNodes)) {
 				if (node.getNodeName().equals(listValueName))
 				{
 					list.add(node);
 					stepList.add(new StepNode(node, this));
+				}
+			}
+			this.parent = parent;
+		}
+
+
+		public StepList(String listValueName, List<Step> childNodes, Step parent) {
+			this.list = new ArrayList<>();
+			this.stepList = new ArrayList<>();
+			this.listValueName = listValueName;
+			for (Step node : childNodes) {
+				if (node.getNodeName().equals(listValueName))
+				{
+					list.add(node.getXmlNode());
+					stepList.add(node);
 				}
 			}
 			this.parent = parent;
@@ -68,6 +87,11 @@ public class XMLStepper {
 			return parent;
 		}
 
+		@Override
+		public String getNodeName() {
+			return listValueName;
+		}
+
 		/**
 		 * @see StepList#forEach
 		 * @see StepList#asList()
@@ -76,7 +100,7 @@ public class XMLStepper {
 		 */
 		@Override
 		@Deprecated
-		public Step getNode(String path) {
+		public Step getNode(String path, Where... where) {
 			return this;
 		}
 
@@ -86,7 +110,7 @@ public class XMLStepper {
 		 */
 		@Override
 		@Deprecated
-		public StepList getList(String path) {
+		public StepList getList(String path, Where... where) {
 			return this;
 		}
 
@@ -124,25 +148,16 @@ public class XMLStepper {
 		public Iterator<Step> iterator() {
 			return stepList.iterator();
 		}
-	}
-
-	public static class StepNONE implements Step {
 
 		@Override
-		public Node getXmlNode() {
-			return null;
+		public String toString() {
+			return "Node list: "+getPath() + "/" + listValueName + "*";
 		}
 
 		@Override
-		public Step getParent() {
-			return null;
+		public String getPath() {
+			return parent.getPath();
 		}
-
-		@Override
-		public void setXmlNode(Node node) {
-
-		}
-
 	}
 
 	@SuppressWarnings("serial")
@@ -181,6 +196,11 @@ public class XMLStepper {
 			this.node = node;
 		}
 
+		@Override
+		public String toString() {
+			return "Node: "+getParent().getPath() + "/" + node.getNodeName();
+		}
+
 	}
 
 	public static class StepDocument implements Step {
@@ -199,7 +219,7 @@ public class XMLStepper {
 
 		@Override
 		public Step getParent() {
-			return new StepNONE();
+			return null;
 		}
 
 		@Override
@@ -210,27 +230,71 @@ public class XMLStepper {
 		public Document getXMLDocument() {
 			return document;
 		}
+
+		@Override
+		public String toString() {
+			return "XML Document with root: "+rootNode.getNodeName();
+		}
+
+		@Override
+		public String getPath() {
+			return "/";
+		}
+	}
+
+	/**
+	 * Util class of static method generating specialized instances.
+	 * @author Zdenek Novotny (DeznekCZ)
+	 * @see Where#atributeMatches(String, String) Where.atributeMatches(name, regex)
+	 * @see Where#atributeEquals(String, String) Where.atributeEquals(name, equaledValue)
+	 * @see Where#atributeEqualsIgnoreCase(String, String) Where.atributeEqualsIgnoreCase(name, equaledValue)
+	 */
+	@SuppressWarnings("serial")
+	public static class Where extends Pair<String, Predicate<String>> {
+
+		private Supplier<String> toString;
+
+		private Where(String key, Predicate<String> value, Supplier<String> toString) {
+			super(key, value);
+			this.toString = toString;
+		}
+
+		public static Where atributeMatches(String attribute, String regex) {
+			return new Where(attribute, string -> string != null && string.matches(regex), () -> String.format("/%s/",regex));
+		}
+
+		public static Where atributeEquals(String attribute, String value) {
+			return new Where(attribute, string -> string != null && string.equals(value), () -> value);
+		}
+
+		public static Where atributeEqualsIgnoreCase(String attribute, String value) {
+			return new Where(attribute, string -> string != null && string.equalsIgnoreCase(value), () -> value);
+		}
+
+		@Override
+		public String toString() {
+			return getKey() + "=" + toString.get();
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			return o instanceof Where && toString().equals(o.toString());
+		}
+
+		@Override
+		public int hashCode() {
+			return toString().hashCode();
+		}
 	}
 
 	public static interface Step {
-		/**
-		 *
-		 * @param path steps to tree node "elem1/elem2/finalElem"
-		 * @throws XMLStepperException
-		 * @return returns final element of path
-		 */
-		public default Step getNode(String path) {
-			if (path.contains("/")) {
-				String rootPath = path.substring(0, path.lastIndexOf('/'));
-				String rootEnclosing = path.substring(path.lastIndexOf('/') + 1, path.length());
-				return getNode(rootPath).getNode(rootEnclosing);
-			} else {
-				for (Node node : ForEach.DOMNodeIterable(getXmlNode().getChildNodes())) {
-					if (node.getNodeName().equals(path))
-						return new StepNode(node, this);
-				}
-			}
-			throw XMLStepperException.notExists(XMLStepperException.ELEMENT,path);
+
+		public default String getNodeName() {
+			return getXmlNode() != null ? getXmlNode().getNodeName() : "#NULL";
+		}
+
+		public default String getPath() {
+			return getParent().getPath() + "/" + getXmlNode().getNodeName();
 		}
 
 		public void setXmlNode(Node node);
@@ -268,21 +332,11 @@ public class XMLStepper {
 			return converter.apply(result.get());
 		}
 
-		public default StepList getList(String path) throws XMLStepperException {
-			if (path.contains("/")) {
-				String rootPath = path.substring(0, path.lastIndexOf('/'));
-				String rootEnclosing = path.substring(path.lastIndexOf('/') + 1, path.length());
-				return getNode(rootPath).getList(rootEnclosing);
-			} else {
-				return new StepList(path, getXmlNode().getChildNodes(), this);
-			}
-		}
-
-		default void collectText(List<String> collector) {
+		public default void collectText(List<String> collector) {
 			collector.add(text());
 		}
 
-		default boolean hasElement(String name)
+		public default boolean hasElement(String name)
 		{
 			if (this.getXmlNode().hasChildNodes())
 				for (Node node : ForEach.DOMNodeIterable(getXmlNode().getChildNodes()))
@@ -292,7 +346,7 @@ public class XMLStepper {
 			return false;
 		}
 
-		default boolean hasAttribute(String name)
+		public default boolean hasAttribute(String name)
 		{
 			if (this.getXmlNode().hasAttributes())
 				for (Node node : ForEach.DOMNodeIterableMap(getXmlNode().getAttributes()))
@@ -303,6 +357,82 @@ public class XMLStepper {
 		}
 
 		public default String text() { return getXmlNode().getTextContent(); }
+
+		/**
+		 * Returns list of nodes which matches where (if defined)
+		 * @param path steps to tree list node "elem1/elem2/finalElem"
+		 * @param where array instances of {@link Where}
+		 * @return new instance of {@link StepList}
+		 * @see Where
+		 */
+		public default StepList getList(String path, Where... where) {
+			if (where == null || where.length == 0) {
+				if (path.contains("/")) {
+					String rootPath = path.substring(0, path.lastIndexOf('/'));
+					String rootEnclosing = path.substring(path.lastIndexOf('/') + 1, path.length());
+					return getNode(rootPath).getList(rootEnclosing);
+				} else {
+					return new StepList(path, getXmlNode().getChildNodes(), this);
+				}
+			} else {
+				StepList list = getList(path);
+				List<Step> steps = new ArrayList<>();
+
+				for (Step step: list.asList()) {
+					boolean accepted = true;
+
+					for (Where attribute : where) {
+						if (!accepted) break;
+						accepted &=
+							   step.hasAttribute(attribute.getKey())
+							&& attribute.getValue().test(step.attribute(attribute.getKey()));
+					}
+
+					if (accepted) steps.add(step);
+				}
+				return new StepList(list.getNodeName(), steps, list.getParent());
+			}
+		}
+
+		/**
+		 * Returns nodes which matches where (if defined)
+		 * @param path steps to tree node "elem1/elem2/finalElem"
+		 * @param where array instances of {@link Where}
+		 * @return new instance of {@link Step}
+		 * @see Where
+		 */
+		public default Step getNode(String path, Where... where) {
+			if (where == null || where.length == 0) {
+				if (path.contains("/")) {
+					String rootPath = path.substring(0, path.lastIndexOf('/'));
+					String rootEnclosing = path.substring(path.lastIndexOf('/') + 1, path.length());
+					return getNode(rootPath).getNode(rootEnclosing);
+				} else {
+					for (Node node : ForEach.DOMNodeIterable(getXmlNode().getChildNodes())) {
+						if (node.getNodeName().equals(path))
+							return new StepNode(node, this);
+					}
+				}
+			} else {
+				for (Step step: getList(path).asList()) {
+					boolean accepted = true;
+
+					for (Where attribute : where) {
+						if (!accepted) break;
+						accepted &=
+							   step.hasAttribute(attribute.getKey())
+							&& attribute.getValue().test(step.attribute(attribute.getKey()));
+					}
+
+					if (accepted) return step;
+				}
+			}
+			return null;
+		}
+
+		public default String getTextContent() {
+			return getXmlNode() != null ? getXmlNode().getTextContent() : "";
+		}
 	}
 
 	public static StepDocument from(Document document) {
